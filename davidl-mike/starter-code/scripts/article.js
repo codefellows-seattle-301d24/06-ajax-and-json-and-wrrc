2a89982e8417a7db56350be13bdc2f488abaca91
Michael Shinners <mishinners@icloud.com>
2017-09-19 11:29:21 -0700

    finished all TODOs

--- davidl-mike/starter-code/scripts/article.js
+++ davidl-mike/starter-code/scripts/article.js
@@ -50,9 +50,9 @@ Article.fetchAll = function() {
     // When rawData is already in localStorage,
     // we can load it with the .loadAll function above,
     // and then render the index page (using the proper method on the articleView object).
-    Article.loadAll(localStorage); //TODO: What do we pass in to loadAll()?
+    Article.loadAll(JSON.parse(localStorage.rawData)); //DONE: What do we pass in to loadAll()?
 
-    //TODO: What method do we call to render the index page?
+    //DONE: What method do we call to render the index page?
     articleView.initIndexPage();
   } else {
     // DONE: When we don't already have the rawData,
