dfabbd30cc48e031f60e869aac02d12ad3272102
Rob Bronson <robert.j.bronson@gmail.com>
2017-09-18 17:01:19 -0700

    finished todo list, added data to local storage, called function in html

--- rob-mark/scripts/article.js
+++ rob-mark/scripts/article.js
@@ -1,5 +1,6 @@
 'use strict';
 
+let rawData = [];
 function Article (rawDataObj) {
   this.author = rawDataObj.author;
   this.authorUrl = rawDataObj.authorUrl;
@@ -8,7 +9,6 @@ function Article (rawDataObj) {
   this.body = rawDataObj.body;
   this.publishedOn = rawDataObj.publishedOn;
 }
-
 // REVIEW: Instead of a global `articles = []` array, let's track this list of all articles directly on the
 // constructor function. Note: it is NOT on the prototype. In JavaScript, functions are themselves
 // objects, which means we can add properties/values to them at any time. In this case, we have
@@ -45,19 +45,29 @@ Article.loadAll = function(rawData) {
 
 // This function will retrieve the data from either a local or remote source,
 // and process it, then hand off control to the View.
+function populateData (response) {
+  localStorage.setItem(rawData, JSON.stringify(response));
+  Article.loadAll(response);
+  articleView.initIndexPage();
+}
 Article.fetchAll = function() {
   if (localStorage.rawData) {
     // When rawData is already in localStorage,
     // we can load it with the .loadAll function above,
     // and then render the index page (using the proper method on the articleView object).
-    Article.loadAll(JSON.parse(localStorage.rawData)); //TODO: What do we pass in to loadAll()?
-    //TODO: What method do we call to render the index page?
+    Article.loadAll(JSON.parse(localStorage.rawData)); //DONE: What do we pass in to loadAll()?
+    //DONE: What method do we call to render the index page?
+    articleView.initIndexPage();
 
   } else {
-    // TODO: When we don't already have the rawData,
+    // DONE: When we don't already have the rawData,
+    $.get(`/data/hackerIpsum.json`, populateData);
+
     // we need to retrieve the JSON file from the server with AJAX (which jQuery method is best for this?),
     // cache it in localStorage so we can skip the server call next time,
     // then load all the data into Article.all with the .loadAll function above,
     // and then render the index page.
+
+
   }
 }
